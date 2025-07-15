a55ef680f9b53a14babf7757e87e147219e8bc7a
Kylian Rekik <kylian@outlook.fr>
2025-07-16 00:17:44 +0200

    correction side barre

--- frontend/src/layout/Sidebar.vue
+++ frontend/src/layout/Sidebar.vue
@@ -1,32 +1,35 @@
-<script setup>
-import SidebarItem from './SidebarItem.vue'
-
-import Dashboard from '../assets/svg/dashboard.svg'
-import Config from '../assets/svg/configuration.svg'
-import Export from '../assets/svg/export.svg'
-import WhiteBoard from '../assets/svg/whiteboard.svg'
-import Restaurant from '../assets/svg/restaurant.svg'
-import Meeting from '../assets/svg/meeting.svg'
-</script>
-
-
+<<<<<<< Updated upstream
+=======
 <template>
   <aside class="flex flex-col items-center bg-color-background text-color-black h-screen border-r border-r-red">
 
     <div class="flex flex-col m-auto justify-center items-center py-4">
-      <router-link to="/" class="text-2xl font-bold text-color-red">
-        <img src="../assets/img/Logo.png" alt="SumãtoSensã Logo" class="w-16 h-16 mb-2 ">
-      </router-link>
+      <img src="../assets/img/Logo.png" alt="SumãtoSensã Logo" class="w-16 h-16 mb-2 ">
       <span class="font-bold">SumãtoSensã</span>
     </div>
 
     <nav class="flex flex-col px-4 py-4 space-y-2">
-      <SidebarItem :icon="Dashboard" label="Dashboard" to="/" />
-      <SidebarItem :icon="Export" label="Export" to="/export" />
-      <SidebarItem :icon="Config" label="Paramètres" to="/config" />
-      <SidebarItem :icon="WhiteBoard" label="Amphithéatre" to="/whiteboard" />
-      <SidebarItem :icon="Restaurant" label="salle de Restauration" to="/restaurant" />
-      <SidebarItem :icon="Meeting" label="Salle de Réunions" to="/meeting" />
+      <SidebarItem :icon="Home" label="Home" to="/" />
+      <SidebarItem :icon="History" label="History" to="/history" />
+      <SidebarItem :icon="Devices" label="Devices" to="/devices" />
+      <SidebarItem :icon="Management" label="Management" to="/management" />
+      <SidebarItem :icon="Export" label="Export" to="/exort" />
+      <SidebarItem :icon="Config" label="Settings" to="/settings" />
+
     </nav>
   </aside>
 </template>
+
+>>>>>>> Stashed changes
+<script setup>
+import SidebarItem from './SidebarItem.vue'
+
+import Home from '../assets/svg/home.svg'
+import Config from '../assets/svg/configuration.svg'
+import Export from '../assets/svg/export.svg'
+import Devices from '../assets/svg/devices.svg'
+import History from '../assets/svg/history.svg'
+import Management from '../assets/svg/management.svg'
+</script>
+
+
