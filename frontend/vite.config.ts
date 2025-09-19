92a771c666b4c852f753a80911d8e62a34b33fc3
Vidocq17 <avidanB1704@gmail.com>
2025-09-19 11:14:00 +0200

    fix: update allowedHosts in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   },
   server: {
-    allowedHosts: ['merely-noted-gannet.ngrok-free.app'],
+    allowedHosts: ['merely-noted-gannet.ngrok-free.app', 'sumatosensa-frontend.onrender.com'],
     host: true,
     port: 5173,
     strictPort: true,
