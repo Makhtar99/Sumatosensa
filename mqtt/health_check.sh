e1978130eceee2d969a2e90e875173496bc3446d
Makhtar99 <127096437+Makhtar99@users.noreply.github.com>
2025-09-22 23:30:33 +0200

    fix: add WebSocket listener and improve MQTT startup timing

--- mqtt/health_check.sh
+++ mqtt/health_check.sh
@@ -2,7 +2,10 @@
 # Start Mosquitto in background
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf -v &
 
+# Wait for Mosquitto to start
+sleep 5
+
 # Start simple HTTP health check server on port 80
 while true; do
-    echo -e "HTTP/1.1 200 OK\r\n\r\nMQTT Service Running" | nc -l -p 80 -q 1
+    echo -e "HTTP/1.1 200 OK\r\n\r\nMQTT Service Running - Ports 1883 & 9001 active" | nc -l -p 80 -q 1
 done
