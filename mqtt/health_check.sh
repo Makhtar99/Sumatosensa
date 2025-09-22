99ee45abc1bfe09726a35a4eab4e18d6e64221d7
Makhtar99 <127096437+Makhtar99@users.noreply.github.com>
2025-09-22 21:22:47 +0200

    fix: add HTTP health check server for MQTT service on Render

---
+++ mqtt/health_check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Start Mosquitto in background
+/usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf -v &
+
+# Start simple HTTP health check server on port 80
+while true; do
+    echo -e "HTTP/1.1 200 OK\r\n\r\nMQTT Service Running" | nc -l -p 80 -q 1
+done
